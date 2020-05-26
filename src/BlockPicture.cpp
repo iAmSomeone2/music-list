22371689f0c313014e8c46a27f65d30bd892e6f3
Brenden Davidson <davidson.brenden15@gmail.com>
2020-05-26 18:22:05 -0500

    Base64 decode works

--- src/BlockPicture.cpp
+++ src/BlockPicture.cpp
@@ -1,3 +1,6 @@
+#include <cstring>
+#include <array>
+
 #include "BlockPicture.hpp"
 
 using namespace MusicList;
@@ -26,35 +29,35 @@ vector<int8_t> BlockPicture::base64Decode(const string& data)
 {
     uint_fast32_t numChars = data.size();
     vector<int8_t> outVec;
-    int8_t workSet[4];
+    outVec.reserve(numChars);
+
+    std::array<string,4> workSet;
     for (uint_fast32_t i = 0; i < numChars; i+=4)
     {
-        // Read the original 4 bytes into the working set.
-        workSet[0] = BASE64_TABLE[data[i]];
-        workSet[1] = BASE64_TABLE[data[i+1]];
-        workSet[2] = BASE64_TABLE[data[i+2]];
-        workSet[3] = BASE64_TABLE[data[i+3]];
+        std::string tmp = "";
+        for (uint8_t j = 0; j < 4; j++)
+        {
+            workSet[j] = BASE64_TABLE[data[i+j]];
 
-        // Shift the first 2 bits off the end.
-        workSet[0] = workSet[0] << 2;
-        workSet[1] = workSet[1] << 2;
-        workSet[2] = workSet[2] << 2;
-        workSet[3] = workSet[3] << 2;
+            if (workSet[j] != "")
+            {
+                workSet[j] = workSet[j].substr(2);
+                tmp.append(workSet[j]);
+            }
+        }
 
-        // Create temporary 32-bit value.
-        int32_t temp = (workSet[0] << 24);
-        temp = temp | (workSet[1] << 18);
-        temp = temp | (workSet[2] << 12);
-        temp = temp | workSet[3] << 6;
-
-        workSet[0] = (temp & 0xFF000000) >> 24;
-        workSet[1] = (temp & 0x00FF0000) >> 16;
-        workSet[2] = (temp & 0x0000FF00) >> 8;
-        workSet[3] = temp & 0x000000FF;
-
-        outVec.push_back(workSet[0]);
-        outVec.push_back(workSet[1]);
-        outVec.push_back(workSet[2]);
+        uint8_t tmpLen = tmp.length();
+        
+        for (uint8_t j = 0; j < 4; j++)
+        {
+            int8_t val;
+            if (j*8 < tmpLen)
+            {
+                std::string byte = tmp.substr(8*j, 8);
+                val = static_cast<int8_t>(strtol(byte.c_str(), nullptr, 2));
+                outVec.push_back(val);
+            }
+        }
     }
 
     return outVec;
