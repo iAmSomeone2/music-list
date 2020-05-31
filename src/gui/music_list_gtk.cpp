8d262f6faf2f060e89b1c0b3f39d9577ace78f8c
Brenden Davidson <davidson.brenden15@gmail.com>
2020-05-30 22:13:59 -0500

    Configured GUI libs

---
+++ src/gui/music_list_gtk.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+#include <gtkmm-3.0/gtkmm.h>
+
+#include <Importer.hpp>
+
+int main(int argc, char* argv[])
+{
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
+}
