f6d1617c9d08b9dfcd84cf85129b2c64cb25de06
Brenden Davidson <davidson.brenden15@gmail.com>
2020-05-30 22:25:14 -0500

    Made building GUI optional

--- src/gui/music_list_gtk.cpp
+++ src/gui/music_list_gtk.cpp
@@ -9,7 +9,9 @@ int main(int argc, char* argv[])
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
     Gtk::Window window;
-    window.set_default_size(200, 200);
+    window.set_default_size(300, 400);
+    window.set_resizable(false);
+    window.set_title("MusicList");
 
     return app->run(window);
 }
